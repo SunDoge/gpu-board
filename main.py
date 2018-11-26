9d9c0fd363bcb5dece39e0b153fb10dd39e35186
SunDoge <384813529@qq.com>
2018-11-26 15:46:38 +0800

    server ok

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from sanic import Sanic, response
-import socketio
 import asyncio
+
+from gpustat import new_query
+
+import config
+import socketio
+from sanic import Sanic, response
 
 app = Sanic()
 sio = socketio.AsyncServer(async_mode='sanic')
@@ -12,15 +16,17 @@ async def index(request):
     return await response.file('index.html')
 
 
-async def nvidia_smi():
+async def update_gpu_stats():
     while True:
         await asyncio.sleep(1)
-        print(len(sio.manager.rooms))
+        # print(len(sio.manager.rooms))
         if len(sio.manager.rooms) > 0:
-            await sio.emit('nvidia-smi', [])
+            gpu_stats = new_query()
+            # print(gpu_stats.jsonify())
+            await sio.emit('gpustat', gpu_stats.jsonify())
         # print(len(app.sio.rooms))
 
 
 if __name__ == "__main__":
-    app.add_task(nvidia_smi())
-    app.run(host="127.0.0.1", port=8000)
+    app.add_task(update_gpu_stats())
+    app.run(host="0.0.0.0", port=8000)
